6179af8c758ff34d358675a9e20489385ccaa4ee
Nicklas Silversved <Nicklas.Silversved@cybercom.com>
2024-04-18 07:45:58 +0200

    Initial Commit

---
+++ nsmdb/nsmdbApp.swift
@@ -0,0 +1,17 @@
+//
+//  nsmdbApp.swift
+//  nsmdb
+//
+//  Created by Nicklas Silversved on 2024-04-18.
+//
+
+import SwiftUI
+
+@main
+struct nsmdbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
